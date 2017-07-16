d4d6cd05bc693ad31b8fcdf48a6fc6b65856cbec
KinshukBasu <kinshukbasu.acc@gmail.com>
2017-07-16 15:32:00 -0400

    Initial commit

---
+++ FlaskApp/static/js/logIn.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('#btnLogin').click(function() {
+
+        $.ajax({
+            url: '/logIn',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
